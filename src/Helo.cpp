c845153475ac0c14515377d9d6f0273853ea64ac
gooooo-create <210103956@qq.com>
2026-07-06 17:04:16 +0800

    init

---
+++ src/Helo.cpp
@@ -0,0 +1,5 @@
+#include "../inc/Helo.h"
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "../inc/Helo.h"
+
+int main() {
+    int result = add(5, 3);
+    std::cout << "The sum is: " << result << std::endl;
+    return 0;
+}
